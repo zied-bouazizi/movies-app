f66cb259f1ed01dad56da255028517f23cd0a4e9
zied-bouazizi <33433235+zied-bouazizi@users.noreply.github.com>
2021-09-23 17:20:12 +0100

    Testing API

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -10,12 +10,6 @@
             <span class="mx-2">|</span>
             <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('M d, Y') }}</span>
         </div>
-        <div class="text-gray-400 text-sm">
-            <span>
-                @foreach ($movie['genre_ids'] as $genre)
-                 {{ $genres->get($genre) }} @if (!$loop->last), @endif
-                @endforeach
-            </span>
-        </div>
+        <div class="text-gray-400 text-sm">@foreach ($movie['genre_ids'] as $genre){{ $genres->get($genre) }}@if(!$loop->last),@endif @endforeach</div>
     </div>
 </div>
