954fc555ed1ff52131e2af55b63b361bc305e98a
zied-bouazizi <33433235+zied-bouazizi@users.noreply.github.com>
2021-09-27 10:29:43 +0100

    Search Dropdown with Livewire

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  
-  <title>Movies App</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @livewireStyles
+
+    <title>Movies App</title>
 </head>
+
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-gray-800">
         <div class="container mx-auto px-4 lg:px-32 py-6 flex flex-col md:flex-row items-center justify-between">
             <ul class="flex flex-col md:flex-row items-center">
                 <li>
                     <a href="{{ route('movies.index') }}" class="flex items-center">
-                      <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm3 2h6v4H7V5zm8 8v2h1v-2h-1zm-2-2H7v4h6v-4zm2 0h1V9h-1v2zm1-4V5h-1v2h1zM5 5v2H4V5h1zm0 4H4v2h1V9zm-1 4h1v2H4v-2z" clip-rule="evenodd"></path></svg>
-                      <h1 class="text-lg font-bold ml-3">Movies App</h1>
+                        <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm3 2h6v4H7V5zm8 8v2h1v-2h-1zm-2-2H7v4h6v-4zm2 0h1V9h-1v2zm1-4V5h-1v2h1zM5 5v2H4V5h1zm0 4H4v2h1V9zm-1 4h1v2H4v-2z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                        <h1 class="text-lg font-bold ml-3">Movies App</h1>
                     </a>
                 </li>
                 <li class="md:ml-16 mt-3 md:mt-0">
@@ -29,25 +36,22 @@
                 </li>
             </ul>
             <div class="flex flex-col md:flex-row items-center">
-                <div class="relative mt-3 md:mt-0">
-                    <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:ring focus:border-gray-300" placeholder="Search">
-                    <div class="absolute top-0">
-                        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
+                <livewire:search-dropdown>
+                    <div class="md:ml-4 mt-3 md:mt-0">
+                        <a href="#">
+                            <img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-8 h-8">
+                        </a>
                     </div>
-                </div>
-                <div class="md:ml-4 mt-3 md:mt-0">
-                    <a href="#">
-                        <img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-8 h-8">
-                    </a>
-                </div>
             </div>
         </div>
     </nav>
     @yield('content')
     <footer class="border border-t border-gray-800">
-      <div class="container mx-auto text-center text-sm px-4 lg:px-32 py-6">
-          Copyright &copy; 2021
-      </div>
-  </footer>
+        <div class="container mx-auto text-center text-sm px-4 lg:px-32 py-6">
+            Copyright &copy; 2021
+        </div>
+    </footer>
+    @livewireScripts
 </body>
+
 </html>
